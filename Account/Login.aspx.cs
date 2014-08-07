4576ec4c73d5ccb6ac2c82a2ac6215908c9b422e
garciamercador <garciamercado.r@gmail.com>
2014-08-07 19:26:48 -0400

    1.1

---
+++ Account/Login.aspx.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System;
+using System.Web;
+using System.Web.UI;
+using WebSite6;
+
+public partial class Account_Login : Page
+{
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            RegisterHyperLink.NavigateUrl = "Register2";
+            OpenAuthLogin.ReturnUrl = Request.QueryString["ReturnUrl"];
+            var returnUrl = HttpUtility.UrlEncode(Request.QueryString["ReturnUrl"]);
+            
+
+            var encriptado = Encriptator.EncryptData("gatiadssaddascxz123sdasdasdzcxzczxcasdasdqweqweqwesaddasn");
+            System.Diagnostics.Debug.WriteLine(encriptado);
+            var dencriptado = Encriptator.DecryptString(encriptado);
+            System.Diagnostics.Debug.WriteLine(dencriptado);
+
+            if (!String.IsNullOrEmpty(returnUrl))
+            {
+                RegisterHyperLink.NavigateUrl += "?ReturnUrl=" + returnUrl;
+            }
+        }
+
+        protected void LogIn(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                // Validate the user password
+                var manager = new UserManager();
+                ApplicationUser user = manager.Find(UserName.Text, Password.Text);
+                if (user != null)
+                {
+                    IdentityHelper.SignIn(manager, user, RememberMe.Checked);
+                    IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
+                }
+                else
+                {
+                    FailureText.Text = "Invalid username or password.";
+                    ErrorMessage.Visible = true;
+                }
+            }
+        }
+}
